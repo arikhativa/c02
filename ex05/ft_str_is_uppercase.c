44d4ab7d2cc69ccd14566772aaf697fecd971422
Yoav Rabby <yrabby@c2r7s3.42madrid.com>
2022-05-19 12:41:44 +0200

    ex05 done

---
+++ ex05/ft_str_is_uppercase.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_str_is_uppercase.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yrabby <yrabby@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/19 10:49:05 by yrabby            #+#    #+#             */
+/*   Updated: 2022/05/19 12:36:00 by yrabby           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	is_uppercase(char ch)
+{
+	return (ch >= 'A' && ch <= 'Z');
+}
+
+int ft_str_is_uppercase(char *str)
+{
+	if (!str)
+		return (1);
+	while (*str)
+	{
+		if (!is_uppercase(*str))
+			return 0;
+		++str;
+	}
+	return (1);
+}
