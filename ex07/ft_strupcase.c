ec9ce4bf37d2b9b3ddfbac15a5475087bbb6a6a1
Yoav Rabby <yrabby@c2r7s3.42madrid.com>
2022-05-19 16:13:46 +0200

    ex07 done

---
+++ ex07/ft_strupcase.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strupcase.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yrabby <yrabby@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/19 15:57:39 by yrabby            #+#    #+#             */
+/*   Updated: 2022/05/19 16:03:04 by yrabby           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	is_lowercase(char ch)
+{
+	return (ch >= 'a' && ch <= 'z');
+}
+
+char *ft_strupcase(char *str)
+{
+	char *ret;
+
+	ret = str;
+	if (!str)
+		return (ret);
+	while (*str)
+	{
+		if (is_lowercase(*str))
+			*str = *str - 32;
+		++str;
+	}
+	return (ret);
+}
