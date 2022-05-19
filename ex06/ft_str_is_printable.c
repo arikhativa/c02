d70a0958162a85043ef8e6bcc6ea735f09fce20f
Yoav Rabby <yrabby@c2r7s3.42madrid.com>
2022-05-19 15:56:53 +0200

    ex06 done

--- ex06/ft_str_is_printable.c
+++ ex06/ft_str_is_printable.c
@@ -1,27 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_str_is_uppercase.c                              :+:      :+:    :+:   */
+/*   ft_str_is_printable.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: yrabby <yrabby@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/05/19 10:49:05 by yrabby            #+#    #+#             */
-/*   Updated: 2022/05/19 15:45:17 by yrabby           ###   ########.fr       */
+/*   Created: 2022/05/19 15:45:10 by yrabby            #+#    #+#             */
+/*   Updated: 2022/05/19 15:49:37 by yrabby           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	is_uppercase(char ch)
+int	is_printable(char ch)
 {
-	return (ch >= 'A' && ch <= 'Z');
+	return (ch >= 33 && ch <= 126);
 }
 
-int ft_str_is_uppercase(char *str)
+int ft_str_is_printable(char *str)
 {
 	if (!str)
 		return (1);
 	while (*str)
 	{
-		if (!is_uppercase(*str))
+		if (!is_printable(*str))
 			return 0;
 		++str;
 	}
